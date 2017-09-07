ef58a08dedeec164a9a4a78ab1828205fa293bc5
Valentin Bonnal <co.merce@hotmail.fr>
2017-09-07 15:53:45 +0200

    Mouvement (3 autres directions)

---
+++ src/toucan/MouvementDescendre.java
@@ -0,0 +1,8 @@
+package toucan;
+
+public class MouvementDescendre extends Mouvement {
+	public MouvementDescendre(int yDepl, int xInit, int yInit) {
+		super(0, yDepl, xInit, yInit);
+
+	}
+}
---
+++ src/toucan/MouvementDroite.java
@@ -0,0 +1,8 @@
+package toucan;
+
+public class MouvementDroite extends Mouvement {
+	public MouvementDroite(int xDepl, int xInit, int yInit) {
+		super(xDepl, 0, xInit, yInit);
+
+	}
+}
