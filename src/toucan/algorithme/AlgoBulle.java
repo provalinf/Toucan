aa94eb6d84ee516b4cc672ee17c332e787425f59
Cyril <cyril_basile@hotmail.fr>
2017-10-05 09:00:17 +0200

    AlgoBulle commentaires Début création des classes d'animations(AffectationCaseCase, AffectationVarCase, ComparaisonCaseCase

--- src/toucan/algorithme/AlgoBulle.java
+++ src/toucan/algorithme/AlgoBulle.java
@@ -1,5 +1,8 @@
 package toucan.algorithme;
 
+import toucan.graphique.animation.AffectationCaseCase;
+import toucan.graphique.animation.AffectationVarCase;
+import toucan.graphique.animation.ComparaisonCaseCase;
 import toucan.modele.Case;
 import toucan.modele.LesCases;
 
@@ -11,27 +14,31 @@ public class AlgoBulle extends Algo {
 
 
     public void trier() {
-		int longueur = lesCases.getNbCases();
-		boolean inversion;
-		do {
-			inversion = false;
-			for (int i = 0; i < longueur - 1; i++) {
-                comparaisonCaseCase.executer(lesCases, i, i+1) ;
-				if (lesCases.getCase(i).getValeur() > lesCases.getCase(i + 1).getValeur()) {
-					swap(lesCases, i, i + 1);
-					inversion = true;
-				}
-			}
-		}
-		while (inversion);
-	}
+        int longueur = lesCases.getNbCases();
+        boolean inversion;
+        do {
+            inversion = false;
+            for (int i = 0; i < longueur - 1; i++) {
+                ComparaisonCaseCase comparaisonCaseCase = new ComparaisonCaseCase();
+                comparaisonCaseCase.executer(lesCases, i, i + 1); // Animation comparaison des cases
+                if (lesCases.getCase(i).getValeur() > lesCases.getCase(i + 1).getValeur()) {
+                    swap(lesCases, i, i + 1);
+                    inversion = true;
+                }
+            }
+        }
+        while (inversion);
+    }
 
     private void swap(LesCases lesCases, int c1, int c2) {
-        affectationVarCase.executer(lesCases, c1) ;
+        AffectationVarCase affectationVarCase = new AffectationVarCase();
+        affectationVarCase.executer(lesCases, c1); // Animation affectation de case
         Case cTemp = lesCases.getCase(c1);
-        affectationCaseCase.executer(lesCases, c1, c2);
+        AffectationCaseCase affectationCaseCase = new AffectationCaseCase();
+        affectationCaseCase.executer(lesCases, c1, c2); // Animation affectation d'une case à une autre
         lesCases.setCase(c1, lesCases.getCase(c2));
-        affectationCaseVar.executer(lesCases, c2) ;
+        AffectationVarCase affectationCaseVar = new AffectationVarCase();
+        affectationCaseVar.executer(lesCases, c2); // Animation affectation de case
         lesCases.setCase(c2, cTemp);
     }
 }
---
+++ src/toucan/graphique/animation/ComparaisonCaseCase.java
@@ -0,0 +1,7 @@
+package toucan.graphique.animation;
+
+/**
+ * Created by Cyril on 05/10/2017.
+ */
+public class ComparaisonCaseCase implements IAnimation {
+}
