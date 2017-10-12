57aeded4e9bdb3e900fe7dc2362f57899796aeae
Valentin Bonnal <co.merce@hotmail.fr>
2017-10-12 16:03:13 +0200

    Revert "(cherry picked from commit e932ac1)" Mouvements uniquement This reverts commit d89b1c5
    (cherry picked from commit b0af407)

--- src/toucan/mouvement/MouvementDroite.java
+++ src/toucan/mouvement/MouvementDroite.java
@@ -1,8 +1,9 @@
 package toucan.mouvement;
 
 public class MouvementDroite extends Mouvement {
-	public MouvementDroite(int xDepl, int xInit, int yInit, int val) {
-		super(xDepl, 0, xInit, yInit, val);
+	public MouvementDroite(int xDepl, int xInit, int yInit) {
+		super(xDepl, 0, xInit, yInit);
 		if (xDepl < 0) throw new AssertionError("xDepl ne doit pas etre négatif");
+
 	}
 }
