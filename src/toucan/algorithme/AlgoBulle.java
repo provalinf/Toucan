c799fd640f2088be2baecf706d2dab72de86dd3c
Cyril <cyril_basile@hotmail.fr>
2017-11-06 17:56:07 +0100

    ComparaisonCaseCase intégré au tri à bulles

--- src/toucan/algorithme/AlgoBulle.java
+++ src/toucan/algorithme/AlgoBulle.java
@@ -3,6 +3,7 @@ package toucan.algorithme;
 import toucan.graphique.animation.AffectationCaseCase;
 import toucan.graphique.animation.AffectationCaseVar;
 import toucan.graphique.animation.AffectationVarCase;
+import toucan.graphique.animation.ComparaisonCaseCase;
 import toucan.modele.Case;
 import toucan.modele.LesCases;
 
@@ -25,7 +26,9 @@ public class AlgoBulle extends Algo {
 			for (int i = 0; i < lesCases.getNbCases() - 1; i++) {
 				Case c1 = lesCases.getCase(i);
 				Case c2 = lesCases.getCase(i + 1);
-
+				ComparaisonCaseCase comparaisonCaseCase = new ComparaisonCaseCase();
+				comparaisonCaseCase.executer(lesCases, i, i + 1);
+				equilibreStable();
 				if (c1.getValeurActuel() > c2.getValeurActuel()) {
 					AffectationVarCase aVC1 = new AffectationVarCase();
 					AffectationCaseCase aCC1 = new AffectationCaseCase();
