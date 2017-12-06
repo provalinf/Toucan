fb3927776d5b50bc430019b12a97be9236126d4a
Cyril <cyril_basile@hotmail.fr>
2017-12-06 15:47:40 +0100

    Correction CompareCaseCase

---
+++ src/toucan/analyse/AnalyseurLexical.java
@@ -0,0 +1,7 @@
+package toucan.analyse;
+
+/**
+ * Created by Cyril on 01/12/2017.
+ */
+public class AnalyseurLexical {
+}
--- src/toucan/arbre/CompareCaseCase.java
+++ src/toucan/arbre/CompareCaseCase.java
@@ -4,15 +4,12 @@ package toucan.arbre;
  * Created by Cyril on 06/12/2017.
  */
 public class CompareCaseCase extends Comparaison {
-    int x, y;
     public CompareCaseCase(int x, int y) {
-        this.x = x;
-        this.y = y;
         getCodeDecore(x, y);
     }
 
     @Override
     public String getCodeDecore(int... indices) {
-        return "ComparaisonCaseCase.executer(LesCases, "+x+", "+y+");\n";
+        return "ComparaisonCaseCase.executer(LesCases, "+indices[0]+", "+indices[1]+");\n";
     }
 }
